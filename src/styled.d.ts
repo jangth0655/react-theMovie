ad1aa1b12d458985e78c7ffdb465bd23a79064f4
JANGTAEHEE <jangth0655@gmail.com>
2022-02-04 00:27:53 +0900

    setting typescirpt and styled-components

---
+++ src/styled.d.ts
@@ -0,0 +1,12 @@
+import "styled-components";
+
+declare module "styled-components" {
+  export interface DefaultTheme {
+    color: {
+      main: string;
+      active: string;
+      darkColor: string;
+      whiteColor: string;
+    };
+  }
+}
---
+++ src/theme.ts
@@ -0,0 +1,19 @@
+import { DefaultTheme } from "styled-components";
+
+export const lightTheme: DefaultTheme = {
+  color: {
+    main: "#00A2FF",
+    active: "#FF57A2",
+    darkColor: "#001559",
+    whiteColor: "#FFFFFF",
+  },
+};
+
+export const darkTheme: DefaultTheme = {
+  color: {
+    main: "asdf",
+    active: "asdf",
+    darkColor: "asdf",
+    whiteColor: "#FFFFFF",
+  },
+};
